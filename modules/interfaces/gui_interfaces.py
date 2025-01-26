6abd7a38f79cff8b95194382ea68226c76869f6f
Balazs Banati <balazs.banati@gmail.com>
2025-01-26 10:51:27 +0100

    add new gui interfaces, split MaskSelector class in MVC style

---
+++ modules/interfaces/gui_interfaces.py
@@ -0,0 +1,20 @@
+# modules/interfaces/display_interface.py
+from abc import ABC, abstractmethod
+
+
+class DisplayInterface(ABC):
+    @abstractmethod
+    def display_image(self, mask):
+        pass
+
+
+class KeyHandlerInterface(ABC):
+    @abstractmethod
+    def handle_key(self, key):
+        pass
+
+
+class MouseHandlerInterface(ABC):
+    @abstractmethod
+    def handle_mouse(self, event, x, y, flags, param):
+        pass
--- modules/mask_selector.py
+++ modules/mask_selector.py
@@ -1,10 +1,31 @@
+# modules/mask_selector_model.py
 import cv2
 import numpy as np
+from modules.interfaces.gui_interfaces import DisplayInterface, MouseHandlerInterface, KeyHandlerInterface
 from modules.utils import add_texts_to_image
 
 
 
-class MaskSelector:
+class MaskSelectorModel:
+    def __init__(self, images):
+        self.images = images
+        self.width, self.height = images[0].shape[:2]
+        self.current_page_index = 0
+        self.current_image = self.images[self.current_page_index].copy()
+        mask = np.zeros((self.width, self.height), np.uint8)
+        self.mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        self.drawing = False
+        self.ix, self.iy = -1, -1
+        self.points = []
+
+    def reset_mask(self):
+        self.mask = cv2.cvtColor(np.zeros((self.width, self.height), np.uint8), cv2.COLOR_GRAY2BGR)
+
+    def get_gray_mask(self):
+        return cv2.cvtColor(self.mask, cv2.COLOR_BGR2GRAY)
+
+
+class MaskSelectorView(DisplayInterface, KeyHandlerInterface, MouseHandlerInterface):
     TEXTS = ["Draw a circle around the object",
              "you want to remove.",
              "Press 'A' to go to the previous page.",
@@ -14,67 +35,72 @@ class MaskSelector:
              "Press 'space' to finish."]
     TEXT_COLOR = (255, 255, 255)
 
-    def __init__(self, images):
-        self.images = images
-        self.width, self.height = images[0].shape[:2]
-        self.current_page_index = 0
-        self.current_image = self.images[self.current_page_index].copy()
-        mask = np.zeros((self.width, self.height), np.uint8)
-        self.mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        self.current_image = None
-        self.drawing = False
-        self.ix, self.iy = -1, -1
-        self.points = []
-        self.texts = MaskSelector.TEXTS
-        self.text_color = MaskSelector.TEXT_COLOR
+    def __init__(self, model):
+        self.model = model
+        self.texts = MaskSelectorView.TEXTS
+        self.text_color = MaskSelectorView.TEXT_COLOR
         self.text_pos = (10, 40)
         self.is_text_shown = True
 
-    def draw_polygone(self, event, x, y, flags, param):
+    def display_image(self, mask=None):
+        display_image = self.model.current_image.copy()
+        if self.is_text_shown:
+            display_image = add_texts_to_image(display_image, self.texts, self.text_pos, self.text_color)
+        cv2.imshow('watermark remover', cv2.addWeighted(display_image, 0.7, self.model.mask, 0.3, 0))
+
+    def handle_key(self, key):
+        if key == ord('a'):
+            self.model.current_page_index = max(0, self.model.current_page_index - 1)
+            self.model.current_image = self.model.images[self.model.current_page_index].copy()
+        elif key == ord('d'):
+            self.model.current_page_index = min(len(self.model.images) - 1, self.model.current_page_index + 1)
+            self.model.current_image = self.model.images[self.model.current_page_index].copy()
+        elif key == ord('r'):
+            self.model.reset_mask()
+        elif key == ord('c'):
+            self.is_text_shown = not self.is_text_shown
+        elif key == 32:
+            return False
+        if key in [ord('a'), ord('d'), ord('r'), ord('c')]:
+            self.display_image()
+        return True
+
+    def handle_mouse(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.drawing = True
-            self.ix, self.iy = x, y
-            self.points.append((x, y))
+            self.model.drawing = True
+            self.model.ix, self.model.iy = x, y
+            self.model.points.append((x, y))
         elif event == cv2.EVENT_MOUSEMOVE:
-            if self.drawing:
-                cv2.line(self.current_image, (self.ix, self.iy), (x, y), (0, 0, 0), 2)
-                self.ix, self.iy = x, y
-                self.points.append((x, y))
+            if self.model.drawing:
+                cv2.line(self.model.current_image, (self.model.ix, self.model.iy), (x, y), (0, 0, 0), 2)
+                self.model.ix, self.model.iy = x, y
+                self.model.points.append((x, y))
         elif event == cv2.EVENT_LBUTTONUP:
-            self.drawing = False
-            cv2.line(self.current_image, (self.ix, self.iy), (x, y), (0, 0, 0), 2)
-            self.points.append((x, y))
-            cv2.fillPoly(self.mask, [np.array(self.points)], (255, 255, 255))
-            self.points.clear()
+            self.model.drawing = False
+            cv2.line(self.model.current_image, (self.model.ix, self.model.iy), (x, y), (0, 0, 0), 2)
+            self.model.points.append((x, y))
+            cv2.fillPoly(self.model.mask, [np.array(self.model.points)], (255, 255, 255))
+            self.model.points.clear()
+        self.display_image()
+
+
+class MaskSelector:
+    def __init__(self, images):
+        self.model = MaskSelectorModel(images)
+        self.view = MaskSelectorView(self.model)
 
     def draw_mask(self):
         cv2.namedWindow('watermark remover')
-        cv2.setMouseCallback('watermark remover', self.draw_polygone)
+        cv2.setMouseCallback('watermark remover', self.view.handle_mouse)
 
-        self.current_image = add_texts_to_image(self.images[self.current_page_index], self.texts, self.text_pos, self.text_color)
+        self.view.display_image()
 
         while True:
             key = cv2.waitKey(1) & 0xFF
-            if key == ord('a'):
-                self.current_page_index = max(0, self.current_page_index - 1)
-            elif key == ord('d'):
-                self.current_page_index = min(len(self.images) - 1, self.current_page_index + 1)
-            elif key == ord('r'):
-                self.mask = cv2.cvtColor(np.zeros((self.width, self.height), np.uint8), cv2.COLOR_GRAY2BGR)
-            elif key == ord('c'):
-                self.is_text_shown = not self.is_text_shown
-            elif key == 32:
+            if not self.view.handle_key(key):
                 break
-
-            if key in [ord('a'), ord('d'), ord('r'), ord('c')]:
-                if self.is_text_shown:
-                    self.current_image = add_texts_to_image(self.images[self.current_page_index], self.texts, self.text_pos, self.text_color)
-                else:
-                    self.current_image = self.images[self.current_page_index].copy()
-
-            cv2.imshow('watermark remover', cv2.addWeighted(self.current_image, 0.7, self.mask, 0.3, 0))
 
         cv2.destroyAllWindows()
 
     def get_gray_mask(self):
-        return cv2.cvtColor(self.mask, cv2.COLOR_BGR2GRAY)
+        return self.model.get_gray_mask()
