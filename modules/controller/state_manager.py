310e4655bcac33b72c73550f57d595cdade7f3ce
Banati Balazs <balazs.banati@gmail.com>
2025-09-23 20:49:52 +0200

    bugfix: undo resets the temporal masks

--- modules/controller/state_manager.py
+++ modules/controller/state_manager.py
@@ -16,12 +16,13 @@ class MaskStateManager:
             return
         self.model.mask_data.redo_stack.append(self.model.mask_data.final_mask.copy())
         self.model.mask_data.final_mask = self.model.mask_data.undo_stack.pop()
-        self.model.mask_data.temp_mask = self.model.mask_data.final_mask.copy()
+        self.model.reset_temp_mask()
 
     def redo(self) -> None:
         if not self.model.mask_data.redo_stack:
             return
         self.model.mask_data.undo_stack.append(self.model.mask_data.final_mask.copy())
         self.model.mask_data.final_mask = self.model.mask_data.redo_stack.pop()
-        self.model.mask_data.temp_mask = self.model.mask_data.final_mask.copy()
+        self.model.reset_temp_mask()
+
 
