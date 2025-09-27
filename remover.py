1a76b0b7d666c127f0ac048ff0414744aff25981
Balazs Banati <balazs.banati@gmail.com>
2025-09-27 22:32:09 +0200

    refactor: update remover.py to use DEFAULT_GUI for improved consistency in GUI type handling

--- remover.py
+++ remover.py
@@ -13,6 +13,8 @@ SAVE_PATH = 'output.pdf'
 # The final PDF will have the same dimensions as the original PDF
 MAX_WIDTH = 900
 MAX_HEIGHT = 800
+DEFAULT_GUI = 'pyqt5'
+# DEFAULT_GUI = 'tkinter'
 
 # Set the DPI for the images, this affects the quality of the final PDF
 DPI = 175
@@ -21,7 +23,7 @@ DPI = 175
 def parse_args():
     parser = argparse.ArgumentParser(description='Remove watermark from PDF.')
     parser.add_argument('pdf_path', type=str, nargs='?', default=PDF_PATH, help='Path to the input PDF file.')
-    parser.add_argument('gui_type', type=str, nargs='?', default='pyqt5', help='Path to save the output PDF file.')
+    parser.add_argument('gui_type', type=str, nargs='?', default=DEFAULT_GUI, help='Path to save the output PDF file.')
     parser.add_argument('--dpi', type=int, default=DPI, help='DPI for the images. Default is 300.')
     parser.add_argument('--max_width', type=int, default=MAX_WIDTH, help=f'Maximum width for the images. Default is {MAX_WIDTH}.')
     parser.add_argument('--max_height', type=int, default=MAX_HEIGHT, help=f'Maximum height for the images. Default is {MAX_HEIGHT}.')
