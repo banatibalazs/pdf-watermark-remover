fee6e0bb57c3f8d2bf949b9164f9aac01dd406b4
Balazs Banati <balazs.banati@gmail.com>
2025-09-27 20:47:05 +0200

    refactor: update mask_manipulator to utilize mask_model for mask operations and state management

--- modules/controller/mask_manipulator.py
+++ modules/controller/mask_manipulator.py
@@ -13,44 +13,44 @@ class MaskManipulator:
         self.model: BaseModel = model
 
     def reset_mask(self) -> None:
-        self.model.reset_mask()
+        self.model.mask_model.reset_mask()
 
     def reset_temp_mask(self) -> None:
         # Set temp mask to all zeros with shape like the final mask
-        self.model.set_temp_mask(np.zeros_like(self.model.get_final_mask()))
+        self.model.mask_model.set_temp_mask(np.zeros_like(self.model.mask_model.get_final_mask()))
 
     def erode_mask(self) -> None:
-        if self.model.get_mode() == MaskMode.THRESHOLD:
-            self.model.set_temp_mask_after_threshold(cv2.erode(self.model.get_temp_mask_after_threshold(),
+        if self.model.config_model.get_mode() == MaskMode.THRESHOLD:
+            self.model.mask_model.set_temp_mask_after_threshold(cv2.erode(self.model.mask_model.get_temp_mask_after_threshold(),
                                                                np.ones((3, 3), np.uint8), iterations=1))
         else:
-            self.model.set_temp_mask(cv2.erode(self.model.get_temp_mask(), np.ones((3, 3),
+            self.model.mask_model.set_temp_mask(cv2.erode(self.model.mask_model.get_temp_mask(), np.ones((3, 3),
                                                                                    np.uint8), iterations=1))
-            self.model.set_final_mask(cv2.erode(self.model.get_final_mask(), np.ones((3, 3),
+            self.model.mask_model.set_final_mask(cv2.erode(self.model.mask_model.get_final_mask(), np.ones((3, 3),
                                                                                      np.uint8), iterations=1))
 
     def dilate_mask(self) -> None:
-        if self.model.get_mode() == MaskMode.THRESHOLD:
-            self.model.set_temp_mask_after_threshold(cv2.dilate(self.model.get_temp_mask_after_threshold(),
+        if self.model.config_model.get_mode() == MaskMode.THRESHOLD:
+            self.model.mask_model.set_temp_mask_after_threshold(cv2.dilate(self.model.mask_model.get_temp_mask_after_threshold(),
                                                                 np.ones((3, 3), np.uint8), iterations=1))
         else:
-            self.model.set_final_mask(cv2.dilate(self.model.get_final_mask(), np.ones((3, 3),
+            self.model.mask_model.set_final_mask(cv2.dilate(self.model.mask_model.get_final_mask(), np.ones((3, 3),
                                                                                       np.uint8), iterations=1))
-            self.model.set_temp_mask(cv2.dilate(self.model.get_temp_mask(), np.ones((3, 3),
+            self.model.mask_model.set_temp_mask(cv2.dilate(self.model.mask_model.get_temp_mask(), np.ones((3, 3),
                                                                                       np.uint8), iterations=1))
 
     def _draw_on_mask(self, color, mask):
-        if self.model.get_cursor_type() == CursorType.CIRCLE:
+        if self.model.mask_model.get_cursor_type() == CursorType.CIRCLE:
             cv2.circle(
                 mask,
-                self.model.get_cursor_pos(),
-                self.model.get_cursor_size(),
+                self.model.mask_model.get_cursor_pos(),
+                self.model.mask_model.get_cursor_size(),
                 [color],
                 -1
             )
-        elif self.model.get_cursor_type() == CursorType.SQUARE:
-            x, y = self.model.get_cursor_pos()
-            size = self.model.get_cursor_size()
+        elif self.model.mask_model.get_cursor_type() == CursorType.SQUARE:
+            x, y = self.model.mask_model.get_cursor_pos()
+            size = self.model.mask_model.get_cursor_size()
             cv2.rectangle(mask,
                           (x - size, y - size),
                           (x + size, y + size),
@@ -58,19 +58,20 @@ class MaskManipulator:
                           -1)
 
     def draw(self):
-        self._draw_on_mask(255, self.model.get_temp_mask())
+        self._draw_on_mask(255, self.model.mask_model.get_temp_mask())
 
     def erase(self):
-        self._draw_on_mask(0, self.model.get_final_mask())
-        self._draw_on_mask(0, self.model.get_temp_mask())
+        self._draw_on_mask(0, self.model.mask_model.get_final_mask())
+        self._draw_on_mask(0, self.model.mask_model.get_temp_mask())
 
     def apply_thresholds(self) -> None:
-        filtered_image = cv2.inRange(self.model.get_current_image_gray(),
-                                            np.array(self.model.get_threshold_min(), dtype=np.uint8),
-                                            np.array(self.model.get_threshold_max(), dtype=np.uint8))
+        filtered_image = cv2.inRange(self.model.image_model.get_current_image_gray(),
+                                            np.array(self.model.config_model.get_threshold_min(), dtype=np.uint8),
+                                            np.array(self.model.config_model.get_threshold_max(), dtype=np.uint8))
 
-        self.model.set_temp_mask_after_threshold(cv2.bitwise_and(filtered_image, self.model.mask_data.temp_mask))
+        self.model.mask_model.set_temp_mask_after_threshold(cv2.bitwise_and(filtered_image, self.model.mask_model.mask_data.temp_mask))
 
     def add_temp_mask_to_final_mask(self):
-        self.model.set_final_mask(cv2.bitwise_or(self.model.get_final_mask(), self.model.get_temp_mask_after_threshold()))
-        self.model.reset_temp_mask()
+        self.model.mask_model.set_final_mask(cv2.bitwise_or(self.model.mask_model.get_final_mask(),
+                                                            self.model.mask_model.get_temp_mask_after_threshold()))
+        self.model.mask_model.reset_temp_mask()
