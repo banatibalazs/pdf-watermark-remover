73991875495c63e99dc58714e4f7792ca70f3486
Balazs Banati <balazs.banati@gmail.com>
2025-10-01 21:28:52 +0200

    feat: add --gui_type argument to specify GUI framework in command line

--- remover.py
+++ remover.py
@@ -23,7 +23,7 @@ DPI = 175
 def parse_args():
     parser = argparse.ArgumentParser(description='Remove watermark from PDF.')
     parser.add_argument('pdf_path', type=str, nargs='?', default=PDF_PATH, help='Path to the input PDF file.')
-    parser.add_argument('gui_type', type=str, nargs='?', default=DEFAULT_GUI, help='Path to save the output PDF file.')
+    parser.add_argument('--gui_type', type=str, nargs='?', default=DEFAULT_GUI, help='Path to save the output PDF file.')
     parser.add_argument('--dpi', type=int, default=DPI, help='DPI for the images. Default is 300.')
     parser.add_argument('--max_width', type=int, default=MAX_WIDTH, help=f'Maximum width for the images. Default is {MAX_WIDTH}.')
     parser.add_argument('--max_height', type=int, default=MAX_HEIGHT, help=f'Maximum height for the images. Default is {MAX_HEIGHT}.')
